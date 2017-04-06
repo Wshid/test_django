333001557934b5ff77f421b8fcba2cc837295761
wshid <kso4013@gmail.com>
2017-04-06 22:54:18 +0900

    Changed the HTML for the site.

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
 # Create your views here.
+
+# render : 넘겨진 요청과 blog/post_list.html 템플릿 받아 리턴된 내용이 브라우저에 보여짐
--- mysite/urls.py
+++ mysite/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'', include('blog.urls')),
+    #파이썬의 경우 정규표현식 작성시 앞에 r을 붙임
 ]
