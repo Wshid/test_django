574f9720e650fe5f87b23387f9140ece1ff9fa21
wshid <kso4013@gmail.com>
2017-04-08 00:14:15 +0900

    Added view and template for detailed post as well as CSS for the site:

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
+
 
 def post_list(request):
     posts=Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts':posts})
         # { }의 내용은 템플릿을 사용하기위해 데이터 추가하는 것
 # Create your views here.
+
+def post_detail(request, pk):
+    post=get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post':post})
 
 # render : 넘겨진 요청과 blog/post_list.html 템플릿 받아 리턴된 내용이 브라우저에 보여짐
